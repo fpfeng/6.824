90fefdceb16e4149ab9ea16098cf45065f07ec3a
feng <187j3x1@gmail.com>
2019-04-08 13:20:26 +0800

    add test log, buggy code sometimes pass TestReElection2A 🍻

--- src/raft/test_test.go
+++ src/raft/test_test.go
@@ -55,20 +55,23 @@ func TestReElection2A(t *testing.T) {
 	cfg.begin("Test (2A): election after network failure")
 
 	leader1 := cfg.checkOneLeader()
-
+	DPrintf("leader1: %d", leader1)
 	// if the leader disconnects, a new one should be elected.
 	cfg.disconnect(leader1)
+	DPrintf("leader1 disconnected")
 	cfg.checkOneLeader()
 
 	// if the old leader rejoins, that shouldn't
 	// disturb the new leader.
 	cfg.connect(leader1)
 	leader2 := cfg.checkOneLeader()
-
+	DPrintf("leader2: %d", leader1)
 	// if there's no quorum, no leader should
 	// be elected.
 	cfg.disconnect(leader2)
+	DPrintf("leader2 disconnected")
 	cfg.disconnect((leader2 + 1) % servers)
+	DPrintf("leader2's next node disconnected")
 	time.Sleep(2 * RaftElectionTimeout)
 	cfg.checkNoLeader()
 
